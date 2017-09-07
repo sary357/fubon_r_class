bf1c8763b7f4f251286b9120c0a9dc5c0ca728a8
Fu-Ming Tsai <sary357@gmail.com>
2017-09-07 10:43:24 +0800

    remove dataset

--- SampleCodes/Day2/test_20170907.R
+++ SampleCodes/Day2/test_20170907.R
@@ -3,3 +3,10 @@ library(readr)
 titanic <- read_csv("https://raw.githubusercontent.com/haven-jeon/introduction_to_most_usable_pkgs_in_project/master/bicdata/data/titanic.csv",
 col_types = cols(survived = col_character()))
 View(titanic)
+
+library(ggplot2)
+library(magrittr)
+ls()
+titanic %>% ggplot(mapping=aes(x=pclass)) + geom_bar()
+
+titanic %>% ggplot(mapping=aes(x=pclass, y=sex)) + geom_boxplot()
